7e635dfe977abe4cf379d47fa8493ff394c16307
dominic white <domw30@gmail.com>
2019-07-19 18:32:55 +0100

    add callbacks to interface.js for increasing and decreasing temperature

--- src/interface.js
+++ src/interface.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  $('#temperature').text(thermostat.temperature);
-})
+
+  $("#temperature").text(thermostat.temperature);
+
+  $("#temperature-up").click(function() {
+    thermostat.up();
+    $("#temperature").text(thermostat.temperature);
+  });
+
+  $("#temperature-down").click(function() {
+    thermostat.down();
+    $("#temperature").text(thermostat.temperature);
+  });
+
+});
