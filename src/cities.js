6065179f31586b42390d5c42d6d6339795c4815d
dominic white <domw30@gmail.com>
2019-07-24 14:16:49 +0100

    add city temperature search bar

--- src/cities.js
+++ src/cities.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp);
+    })
+  })
+
   $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
     $('#current-temperature-london').text(data.main.temp);
   });
